3733a28bfb3819767f6f25e61d1a79d6c0ea4639
LitoMore <litomore@gmail.com>
2024-10-08 21:54:26 +0800

    Fix data order

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,7 @@ use wasm_bindgen::prelude::*;
 pub fn simplify(points: JsValue, tolerance: f32, high_quality: bool) -> JsValue {
     let p: Vec<(f32, f32)> = serde_wasm_bindgen::from_value(points).unwrap();
     let p: Vec<Point<2, f32>> = p.into_iter().map(|(x, y)| point!(x, y)).collect();
-    let mut p = simp(&p, tolerance, high_quality);
-    p.reverse();
+    let p = simp(&p, tolerance, high_quality);
     let p: Vec<(f32, f32)> = p.into_iter().map(|p| (p.vec[0], p.vec[1])).collect();
     serde_wasm_bindgen::to_value(&p).unwrap()
 }
